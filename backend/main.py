39f77faee25c01bf6abbfdb7d7f5a1ec84f7d3e7
spg0wda <166976041+spg0wda@users.noreply.github.com>
2026-06-04 21:33:50 +0530

    Add working FastAPI backend

---
+++ backend/main.py
@@ -0,0 +1,9 @@
+﻿from fastapi import FastAPI
+
+app = FastAPI(title="DeskMindAI API")
+
+@app.get("/")
+def home():
+    return {
+        "message": "DeskMindAI Backend Running Successfully"
+    }
